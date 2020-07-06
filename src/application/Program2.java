9ddbfaa48890dcbb5210969bb33aaa081cf2f69f
Enzo Falvo <bestwordno@gmail.com>
2020-07-06 15:04:22 -0300

    Update implementation - class DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,25 +10,31 @@ import model.entities.Department;
 public class Program2 {
 
     public static void main(String[] args) {
-        
+
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("=== TEST 1: seller findById =====");
+        System.out.println("=== TEST 1: department findById =====");
         Department department = departmentDao.findById(2);
         System.out.println(department);
-        
-        System.out.println("\n=== TEST 2: seller findAll =====");
-        System.out.println("\n=== TEST 3: seller findAll =====");
-        List <Department> list = departmentDao.findAll();
-        
-        for (Department obj : list ) {
+
+        System.out.println("\n=== TEST 2: department findAll =====");
+        List<Department> list = departmentDao.findAll();
+
+        for (Department obj : list) {
             System.out.println(obj);
         }
-        
-        System.out.println("\n=== TEST 3: seller insert =====");
+
+        System.out.println("\n=== TEST 3: department insert =====");
         Department newDepartment = new Department(null, "Music");
         departmentDao.insert(newDepartment);
         System.out.println("Inserted! new department: " + newDepartment.getId());
+
+        System.out.println("\n=== TEST 4: department update =====");
+        Department department2 = departmentDao.findById(6);
+        department.setName("Food");
+        departmentDao.update(department2);
+        System.out.println("Update completed!");
+        
 
     }
 }
